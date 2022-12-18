b654c9a5f3c6c2edb3e69185d4e4b60b48184914
KristinaKac <kachalkovakristina@mailru>
2022-12-18 18:12:37 +0300

    new commit

---
+++ src/test/java/ru/netology/demo/DemoApplicationTests.java
@@ -0,0 +1,30 @@
+package ru.netology.demo;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+    private static GenericContainer<?> myapp = new GenericContainer("devapp");
+    private static GenericContainer<?> myapp2 = new GenericContainer("prodapp");
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeAll
+    public static void setUp() {
+        myapp.start();
+        myapp2.start();
+    }
+
+    @Test
+    void contextLoads() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + myapp.getMappedPort(8099), String.class);
+        System.out.println(forEntity.getBody());
+    }
+
+}
